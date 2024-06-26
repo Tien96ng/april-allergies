c9619df835002a5bee2f82c8cc50cb77085d2c99
Tien Nguyen <tien@Tiens-MacBook-Pro.local>
2024-06-26 14:43:10 -0700

    Display json allergy data to screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Allergies from './components/Allergies';
 import './App.css'
 
+import allergies from "../data/april_allergies.json"
+
 function App() {
 
   return (
     <>
-      <Allergies />
+      <Allergies data={allergies}/>
     </>
   )
 }
--- src/components/Allergies.jsx
+++ src/components/Allergies.jsx
@@ -1,7 +1,56 @@
-export default function Allergies() {
+export default function Allergies({ data }) {
+    const env = data.environmental;
+    const food = data.food;
+
+    const displayLevels = x => {
+        switch(x) {
+            case "level_1":
+                return ["Level 1", env[x].description.toString(), env[x].items]
+            case "level_2":
+                return ["Level 2", env[x].description.toString(), env[x].items]
+            case "level_3":
+                return ["Level 3", env[x].description.toString(), env[x].items]
+            default:
+                return ["Normal", env[x].description.toString(), env[x].items]
+        }
+    }
+
     return(
         <>
-            <h1>Chicken</h1>
+            <h1>Environmental Allergies</h1>
+            {console.log(food)}
+            {Object.keys(env).map(a => {
+                // return <p key={a}>{displayLevels(a)}</p>
+                return(
+                    <div key={a}>
+                        <h2>{displayLevels(a)[0]}</h2>
+                        <p>{displayLevels(a)[1]}</p>
+                        <ul>
+                            {displayLevels(a)[2].map(b => {
+                                return <li key={b}>{b}</li>
+                                })}
+                        </ul>
+                    </div>
+                )
+            })}
+
+            <h1>Food Allergies</h1>
+            {console.log(food)}
+            {Object.keys(food).map(a => {
+                // return <p key={a}>{displayLevels(a)}</p>
+                return(
+                    <div key={a}>
+                        <h2>{displayLevels(a)[0]}</h2>
+                        <p>{displayLevels(a)[1]}</p>
+                        <ul>
+                            {displayLevels(a)[2].map(b => {
+                                return <li key={b}>{b}</li>
+                                })}
+                        </ul>
+                    </div>
+                )
+            })}
+
         </>
     )
 }
